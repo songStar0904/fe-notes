6d74b3c3c30e2c13ec6b9670cbd58de8d351e784
songStar0904 <1043328710@qq.com>
2021-04-20 18:27:14 +0800

    add type

---
+++ JavaScript/type.js
@@ -0,0 +1,40 @@
+// 1. 类型判断
+function type(val) {
+  Object.prototype.toString.call(val).match(/\s+(\w+)/)[1]
+}
+
+type(new Set()) // Set
+
+// 2. 手写Number.isNaN ps: window.isNaN对字符串等都为false
+Number.myIsNaN = (val) => {
+  return typeof val === 'number' && isNaN(val)
+}
+isNaN('a') // true
+Number.myIsNaN('a') // false
+Number.isNaN('a') // false
+
+// 3. 手写instanceof
+function myInstanceof(left, right) {
+  let proto = left.__proto__
+  let prototype = right.prototype
+  while (true) {
+    if (proto === prototype) return true
+    if (proto === null) return false
+    proto = proto.__proto__
+  }
+}
+
+myInstanceof(1, Number)
+
+// 4. 变量提升
+var a = 1
+function bar(a) {
+  a = 2
+  console.log(a) // 2
+  var a = 3
+  console.log(a) // 3
+  function a() {} // 优先级没有var高
+  console.log(a) // 3
+}
+bar(a)
+console.log(a) // 1
